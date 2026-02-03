c65e08db494cbae208513d77e510d7ea57b429ac
王子健 <wangzijian@bonbonwork.com>
2026-02-03 16:23:30 +0800

    v0.0.1

--- internal/pkg/di/web.go
+++ internal/pkg/di/web.go
@@ -3,6 +3,7 @@ package di
 import (
 	"github.com/GoSimplicity/template/internal/api/http"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 // InitWeb 初始化web服务
@@ -10,10 +11,27 @@ func InitWeb(
 	m []gin.HandlerFunc,
 	templateHandler *http.TemplateHandler,
 ) *gin.Engine {
-	server := gin.Default()
+	setGinModeFromConfig()
+
+	server := gin.New()
+	server.Use(gin.Recovery())
 	server.Use(m...)
 
 	templateHandler.RegisterRoutes(server)
 
 	return server
+}
+
+func setGinModeFromConfig() {
+	mode := viper.GetString("server.mode")
+	switch mode {
+	case gin.ReleaseMode, gin.DebugMode, gin.TestMode:
+		gin.SetMode(mode)
+	default:
+		if mode == "" {
+			gin.SetMode(gin.DebugMode)
+			return
+		}
+		gin.SetMode(gin.DebugMode)
+	}
 }
